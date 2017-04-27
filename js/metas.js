1407e583a674174e79ce84a93e081d3a1529d211
root <root@localhost.localdomain>
2017-04-26 21:15:22 -0700

    Shares

---
+++ js/metas.js
@@ -0,0 +1,17 @@
+﻿var app = angular.module('app',[]);
+
+app.service('metaService', function() {
+    var title = 'PLP Comunicación';
+    var metaDescription = '';
+    var metaKeywords = '';
+    return {
+        set: function(newTitle, newMetaDescription, newKeywords) {
+            metaKeywords = newKeywords;
+            metaDescription = newMetaDescription;
+            title = newTitle;
+        },
+        metaTitle: function(){ return title; },
+        metaDescription: function() { return metaDescription; },
+        metaKeywords: function() { return metaKeywords; }
+    }
+});
